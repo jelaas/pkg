63f049e6ad13b961ae4f05963f591f3e1d9d4026
Jens Låås <jens@jelaas.eu>
2016-08-15 16:40:24 +0200

    workbuffer plus fn to write tail

--- dcf.h
+++ dcf.h
@@ -24,12 +24,14 @@ struct dcf {
   int fd;
   char collectiontype[4];
   struct sha256_ctx sha256; /* initialized by dcf_init and dcf_checksum_read|write */
+  struct bigint recordsize;
+  struct bigint temp, temp2;
 };
 
 #define DCF_MAGIC "%DCF_%"
 #define DCF_VERSION 1
 
-int dcf_init(struct dcf *dcf, int fd, const char *collectiontype);
+int dcf_init(struct dcf *dcf, int fd, const char *collectiontype, char *workbuf, int workbufsize); /* workbuf must be big enough to handle maximum recordsize of collection times three (64 bytes should be enough) */
 
 int dcf_magic_read(struct dcf *dcf);
 int dcf_collectiontype_read(struct dcf *dcf);
@@ -47,3 +49,4 @@ int dcf_meta_write_final(struct dcf *dcf);
 int dcf_data_write(struct dcf *dcf, const char *buf, int size);
 int dcf_data_write_final(struct dcf *dcf);
 int dcf_checksum_write(struct dcf *dcf, char *chksum); /* copy of checksum also written to chksum */
+int dcf_recordsize_write(struct dcf *dcf, char *chksum); /* writes recordsize and checksum of recordsize. copy of checksum to chksum */
